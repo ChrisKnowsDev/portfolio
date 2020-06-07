69fc0aca6e29c6296801c7bf88b5c1002be23cb7
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-07 14:12:34 -0400

    sec-b about text complete and styling for mobile

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,3 +2,31 @@
 function closeNav() {
   document.getElementById('nav-toggle').checked = false;
 }
+
+// Hide and show menu on scroll
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-50px';
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const { hash } = this;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
