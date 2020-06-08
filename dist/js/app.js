bb99340f4a508258e6e00db1a33345f8eb136a14
ChrisKnowsDev <chrisharley81@gmail.com>
2020-06-07 22:06:57 -0400

    fixed closeNav function

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,30 +3,6 @@ function closeNav() {
   document.getElementById('nav-toggle').checked = false;
 }
 
-// // Sticky Menu
-// window.addEventListener("scroll", function() {
-//   if (window.scrollY > 150) {
-//     document.querySelector("#navbar").style.backgroundColor = "#fff";
-//     document.querySelector("#navbar").style.color = "#333";
-//     let navLinks = document.querySelectorAll("a");
-//     navLinks.forEach(function(link) {
-//       link.style.color = "#333";
-//     });
-//   } else {
-//     document.querySelector("#navbar").style.backgroundColor = "transparent";
-//     document.querySelector("#navbar").style.color = "#fff";
-//     let navLinks = document.querySelectorAll("a");
-//     navLinks.forEach(function(link) {
-//       link.style.color = "#fff";
-//     });
-//   }
-// });
-
-// Close Menu Overlay
-function closeNav() {
-  document.getElementById("navi-toggle").checked = false;
-}
-
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
   if (this.hash !== '') {
